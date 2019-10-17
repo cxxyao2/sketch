4d8e7f3132e20246fd28b4e64edd318859b1cb20
cxxyao2 <cxxyao2@gmail.com>
2019-10-17 16:18:43 -0400

    modify classname&data

--- frontend/src/view/components/home/tagbasiclist-filter.tsx
+++ frontend/src/view/components/home/tagbasiclist-filter.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { TagBasicList, TagListType } from './tagbasic-list';
 import './tagbasiclist-filter.scss';
 
-
 export class TagBasicListFilter extends React.Component<{
   taglist:TagListType[];
   onBack:() => void;
